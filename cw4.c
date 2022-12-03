c9aa50f8ca9efba43e0811745e921dc6cc9d46f9
Amirjaz <119691206+Amirjaz@users.noreply.github.com>
2022-12-03 11:42:12 +0330

    fourth commit

---
+++ cw4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+     int max=0;
+    int min=100000000;
+    int inpt, n=0;
+    float sum=0;
+    while(1)
+    {
+        scanf("%d", &inpt);
+        if(inpt==-1){
+            break;
+        }
+        if(inpt<min)
+            {
+            min=inpt;
+        }
+        if(inpt>max)
+        {
+            max=inpt;
+        }
+        sum+= inpt;
+
+        n++;
+
+    }
+    printf("%d\n", min);
+    printf("%d\n", max);
+    printf("%.2f", (float)(sum/n));
+
+
+    return 0;
+}
